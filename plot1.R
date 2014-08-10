b7c2c9ff02c04955300aadf41efb34f24e62d956
Martin Hieronymus <martin.hieronymus@nordakademie.de>
2014-08-10 21:03:48 +0200

    First Plot
    This is the first strike

---
+++ plot1.R
@@ -0,0 +1,19 @@
+#Copy the text file to your wd (check with getwd())
+data <- "./household_power_consumption.txt"
+
+#Reading the dates from 2007-02-01 & 2007-02-02
+firstRow_1_2_2007 <- 66637
+lastRow_2_2_2007 <- 69517
+nRow_dif <- lastRow_2_2_2007 - firstRow_1_2_2007
+hpc <- read.table(data,sep=";", header=FALSE, na.strings="?",skip =firstRow_1_2_2007, nrows = nRow_dif)
+
+#Set names
+names(hpc) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+#Code for reading data
+hpc$Date<- as.POSIXct(strptime(paste(hpc$Date,hpc$Time), "%d/%m/%Y %H:%M:%S"))
+
+#Drawing plot1.png
+png("plot1.png")
+hist(hpc$Global_active_power,main="Global Active Power",col=2,xlab="Global Active Power (kilowatts)")
+dev.off()
